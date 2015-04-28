ddb195bb757eb5a5cd187e6afd3b9ab369db5ac5
Chul Kwon <chulk90+github@gmail.com>
2015-04-27 23:09:17 -0400

    first new format

---
+++ portfolio_1.php
@@ -0,0 +1,200 @@
+<li class="mix c_video">
+						<figure>
+							<a class="pp_video" href="http://www.youtube.com/embed/617ANIA5Rqs?rel=0&amp;wmode=transparent" title="">
+								<img src="images/portfolio/tumbnail_1.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Game Center</h3>
+								<span>video</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_photography">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_2.jpg" title="">
+								<img src="images/portfolio/tumbnail_2.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>photography</span>
+							</figcaption>
+						</figure>
+					</li>
+					
+					<li class="mix c_webdesign">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_3.jpg" title="">
+								<img src="images/portfolio/tumbnail_3.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Safari</h3>
+								<span>web design</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_brochure">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_4.jpg" title="">
+								<img src="images/portfolio/tumbnail_4.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>brochure</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_video">
+						<figure>
+							<a class="pp_video" href="http://www.youtube.com/embed/617ANIA5Rqs?rel=0&amp;wmode=transparent" title="">
+								<img src="images/portfolio/tumbnail_5.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Game Center</h3>
+								<span>video</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_photography">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_6.jpg" title="">
+								<img src="images/portfolio/tumbnail_6.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>photography</span>
+							</figcaption>
+						</figure>
+					</li>
+					
+					<li class="mix c_webdesign">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_7.jpg" title="">
+								<img src="images/portfolio/tumbnail_7.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Safari</h3>
+								<span>web design</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_brochure">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_8.jpg" title="">
+								<img src="images/portfolio/tumbnail_8.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>brochure</span>
+							</figcaption>
+						</figure>
+					</li>
+					
+					<li class="mix c_webdesign">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_9.jpg" title="">
+								<img src="images/portfolio/tumbnail_9.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Safari</h3>
+								<span>web design</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_brochure">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_10.jpg" title="">
+								<img src="images/portfolio/tumbnail_10.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>brochure</span>
+							</figcaption>
+						</figure>
+					</li>
+					
+					<li class="mix c_photography">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_11.jpg" title="">
+								<img src="images/portfolio/tumbnail_11.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Game Center</h3>
+								<span>photography</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_photography">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_12.jpg" title="">
+								<img src="images/portfolio/tumbnail_12.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>photography</span>
+							</figcaption>
+						</figure>
+					</li>
+					
+					<li class="mix c_webdesign">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_13.jpg" title="">
+								<img src="images/portfolio/tumbnail_13.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Safari</h3>
+								<span>web design</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_brochure">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_14.jpg" title="">
+								<img src="images/portfolio/tumbnail_14.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Phone</h3>
+								<span>brochure</span>
+							</figcaption>
+						</figure>
+					</li>
+					<li class="mix c_photography">
+						<figure>
+							<a class="pp_image" href="images/portfolio/tumbnail_15.jpg" title="">
+								<img src="images/portfolio/tumbnail_15.jpg" alt="img06">
+								<span class="plus_icon"></span>
+								<span class="img_bg"></span>
+							</a>
+							<figcaption>
+								<h3>Game Center</h3>
+								<span>photography</span>
+							</figcaption>
+						</figure>
+					</li>
